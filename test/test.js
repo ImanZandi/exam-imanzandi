d02ff747bd3cb995d29c56ee3bb3099b725c75be
Iman Zandi <i99zandi@hotmail.com>
2018-12-09 01:18:29 +0330

    tested

--- test/test.js
+++ test/test.js
@@ -3,17 +3,52 @@ const chai = require('chai');
 let assert = chai.assert;
 let _ = require('lodash');
 
-describe('Check user input', function() {
-    it('change and delete property of user obj', function() {
-
+describe('Tamrin', function() {
+    it('Calculator', function() {
+        let calculator = {
+            //a: "",
+            //b: "",
+            sum(num2 = 0) {
+                this.b = num2;
+                return this.a + this.b;
+            },
+            mul(num3 = 0) {
+                return num3 * (this.a + this.b);
+                //return this.a * this.b;
+            },
+            read(num1 = 0) {
+                this.a = num1;
+                //this.a = +prompt('a?', 0);
+                //this.b = +prompt('b?', 0);
+            }
+        };
+        calculator.read(4);
+        assert.strictEqual(calculator.sum(4), 8);
+        assert.strictEqual(calculator.mul(2), 16);
+        //console.log(calculator.sum(4));
+        //console.log(calculator.mul(2));
     });
-    it('check is empty obj?', function() {
-        
-    });
-    it('sum of object properties', function() {
-        
-    });
-    it('multiplication properties of object that is only a number', function() {
-            
+    it('Counter', function() {
+        let ladder = {
+            step: 0,
+            up() {
+                this.step++;
+                return this; //return an object
+            },
+            down() {
+                this.step--;
+                return this;
+            },
+            showStep: function() { // shows the current step
+                //console.log(this.step);
+                assert.strictEqual(this.step, 1);
+                return this;
+            }
+        };
+        ladder.up().up().down().showStep(); // 1
+        /*ladder.up();
+        ladder.up();
+        ladder.down();
+        ladder.showStep(); // 1*/
     });
 });
