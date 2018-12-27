8f6693cf0a048502ec3af82bbf054e9314d20721
Iman Zandi <i99zandi@hotmail.com>
2018-12-27 12:34:19 +0330

    modify

--- test/test.js
+++ test/test.js
@@ -2,6 +2,197 @@ const chai = require('chai');
 let assert = chai.assert;
 let _ = require('lodash');
 
+function camelize(str) {
+    return str
+    .split('-') // my-long-word -> ['my', 'long', 'word']
+    .map(
+        (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+        ) // ['my', 'long', 'word'] -> ['my', 'Long', 'Word']
+    .join(''); // ['my', 'Long', 'Word'] -> myLongWord
+}
+describe('camelize', function() {
+    it('test', function() {
+        assert.strictEqual(camelize("background-color"), 'backgroundColor');
+    });
+    it('test', function() {
+        assert.strictEqual(camelize("list-style-image"), 'listStyleImage');
+    });
+    it('test', function() {
+        assert.strictEqual(camelize("-webkit-transition"), 'WebkitTransition');
+    });
+});
+
+
+function filterRange(arr, a, b) {
+    // added brackets around the expression for better readability
+    return arr.filter(item => (a <= item && item <= b));
+}
+describe('filterRange', function() {
+    let arr = [5, 3, 8, 1];
+    let filtered = filterRange(arr, 1, 4);
+    it('test', function() {
+        _.isEqual(filtered, [3, 1]);
+    });
+    it('test', function() {
+        _.isEqual(arr, [5, 3, 8, 1]);
+    });
+});
+
+
+function filterRangeInPlace(arr, a, b) {
+    for (let i = 0; i < arr.length; i++) {
+      let val = arr[i];
+      // remove if outside of the interval
+      if (val < a || val > b) {
+        arr.splice(i, 1);
+        i--;
+      }
+    } 
+}
+describe('filterRangeInPlace', function() {
+    let arr = [5, 3, 8, 1];
+    filterRangeInPlace(arr, 1, 4); // removed the numbers except from 1 to 4
+    it('test', function() {
+        _.isEqual(arr, [3, 1]);
+    });
+});
+
+describe('sort array large to small', function() {
+    let arr = [5, 2, 1, -10, 8];
+    arr.sort((a, b) => b - a);
+    it('test', function() {
+        _.isEqual(arr, [8, 5, 2, 1, -10]);
+    });
+});
+
+
+function copySorted(arr) {
+    return arr.slice().sort();
+}
+describe('copySorted', function() {
+    let arr = ["HTML", "JavaScript", "CSS"];
+    let sorted = copySorted(arr);
+    it('test', function() {
+        _.isEqual(sorted, ['CSS', 'HTML', 'JavaScript']);
+    });
+    it('test', function() {
+        _.isEqual(arr, ['HTML', 'JavaScript', 'CSS']); //(no changes)
+    });
+});
+
+
+function Calculator() {
+    let methods = {
+        "-": (a, b) => a - b,
+        "+": (a, b) => a + b
+    };
+    this.calculate = function(str) {
+        let split = str.split(' ');
+        let a = +split[0];
+        let op = split[1];
+        let b = +split[2];
+        if (!methods[op] || isNaN(a) || isNaN(b)) {
+            return NaN;
+        }
+        return methods[op](a, b);
+    }
+    this.addMethod = function(name, func) {
+        methods[name] = func;
+    };
+}
+describe('Calculator', function() {
+    let calc = new Calculator;
+    let powerCalc = new Calculator;
+    powerCalc.addMethod("*", (a, b) => a * b);
+    powerCalc.addMethod("/", (a, b) => a / b);
+    powerCalc.addMethod("**", (a, b) => a ** b);
+    it('test', function() {
+        assert.strictEqual(calc.calculate("3 + 7"), 10);
+    });
+    it('test', function() {
+        assert.strictEqual(powerCalc.calculate("2 ** 3"), 8);
+    });
+});
+
+
+describe('map for access obj props', function() {
+    let john = { name: "John", age: 25 };
+    let pete = { name: "Pete", age: 30 };
+    let mary = { name: "Mary", age: 28 };
+    let users = [ john, pete, mary ]; //var items
+    let names = users.map(item => item.name);
+    it('test', function() {
+        _.isEqual(names, ['John', 'Pete', 'Mary']);
+    });
+});
+
+
+function sortByName(arr) {
+    arr.sort((a, b) => b.name > a.name ? 1 : -1);
+}
+describe('sortByName', function() {
+    let john = { name: "John", age: 25 };
+    let pete = { name: "Pete", age: 30 };
+    let mary = { name: "Mary", age: 28 }; 
+    let arr = [ john, pete, mary ]; 
+    sortByName(arr); 
+    it('test', function() {
+        assert.strictEqual(arr[1].name, "Mary");
+    });
+});
+
+
+function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+}  
+describe('shuffle', function() {
+    let arr = [1, 2, 3];
+    shuffle(arr);
+    let cond =
+    _.isEqual(arr, [1, 2, 3]) ||
+    _.isEqual(arr, [1, 3, 2]) ||
+    _.isEqual(arr, [2, 1, 3]) ||
+    _.isEqual(arr, [2, 3, 1]) ||
+    _.isEqual(arr, [3, 1, 2]) ||
+    _.isEqual(arr, [3, 2, 1]);
+    it('test', function() {
+        assert.isTrue(cond);
+    });
+});
+
+
+function getAverageAge(users) {
+    return users.reduce((prev, user) => prev + user.age, 0) / users.length;
+}
+describe('getAverageAge', function() {
+    let john = { name: "John", age: 25 };
+    let pete = { name: "Pete", age: 30 };
+    let mary = { name: "Mary", age: 29 };
+    let arr = [ john, pete, mary ];
+    it('test', function() {
+        assert.strictEqual(getAverageAge(arr), 28);
+    });
+});
+
+
+function unique(arr) {
+    let result = [];
+    for (let str of arr) {
+        if (!result.includes(str)) {
+            result.push(str);
+        }
+    }  
+    return result;
+}
+describe('give unique items', function() {
+    let strings = [
+        "Hare", "Krishna", "Hare", "Krishna", "Krishna", "Krishna", "Hare", "Hare", ":-O"
+    ];  
+    it('test', function() {
+        _.isEqual(unique(strings), ['Hare', 'Krishna', ':-O']);
+    });
+});
+
 describe('styles', function() {
     let styles = [
             "Jazz",
