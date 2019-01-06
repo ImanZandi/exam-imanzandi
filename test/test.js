ce76ea98da2d06519364203853f469873e331b8f
Hamed Qaderi <hamedghaderii@gmail.com>
2019-01-06 11:59:15 +0330

    Debugged

--- test/test.js
+++ test/test.js
@@ -2,43 +2,48 @@ const chai = require('chai');
 let assert = chai.assert;
 let _ = require('lodash');
 
-function strReverse(str) {
-    let arr = str.split('');
-    return arr.reverse().join('');
+function strReverse (str) {
+  let arr = str.split('');
+  return arr.reverse().join('');
 }
-describe('strReverse', function() {
-    it('test', function() {
-        assert.strictEqual(strReverse('coderbyte'), 'etybredoc');
-    });
-    it('test', function() {
-        assert.strictEqual(strReverse('I Love Code'), 'edoC evoL I');
-    });
+describe('strReverse', function () {
+  it('reverses coderbyte', function () {
+    assert.strictEqual(strReverse('coderbyte'), 'etybredoc');
+  });
+  it('reverses I Love Code', function () {
+    assert.strictEqual(strReverse('I Love Code'), 'edoC evoL I');
+  });
+  it('reverses h4', () => {
+    assert.strictEqual(strReverse('h4'), '4h');
+  });
 });
 
-
-function LetterChanges(str) { 
-    let alphabet = "abcdefghijklmnopqrstuvwxyz";
-    let newStr = "";
-    let loc;
-    for (let i = 0; i < str.length; i++) {
-        loc = alphabet.indexOf(str[i]);
-        if (loc === 25) {
-            newStr = newStr + "a";
-        } else if (loc === -1) {
-            newStr = newStr + str[i];
-        } else {          
-            newStr = newStr + alphabet[loc + 1];
-        }
-    } 
-    return newStr.replace(/[aeiou]/g, function(letter) {
-        return letter.toUpperCase();
-    });         
+function LetterChanges (str) {
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  let newStr = '';
+  let loc;
+  for (let i = 0; i < str.length; i++) {
+    loc = alphabet.indexOf(str[i]);
+    if (loc === 25) {
+      newStr = newStr + 'a';
+    } else if (loc === -1) {
+      newStr = newStr + str[i];
+    } else {
+      newStr = newStr + alphabet[loc + 1];
+    }
+  }
+  return newStr.replace(/[aeiou]/g, function (letter) {
+    return letter.toUpperCase();
+  });
 }
-describe('LetterChanges', function() {
-    it('test', function() {
-        assert.strictEqual(LetterChanges('hello*3'), 'Ifmmp*3');
-    });
-    it('test', function() {
-        assert.strictEqual(LetterChanges('fun times!'), 'gvO Ujnft!');
-    });
+describe('LetterChanges', function () {
+  it('checks letter changes hello*3', function () {
+    assert.strictEqual(LetterChanges('hello*3'), 'Ifmmp*3');
+  });
+  it('checks letter changes: fun times!', function () {
+    assert.strictEqual(LetterChanges('fun times!'), 'gvO Ujnft!');
+  });
+  it('checks letter changes', () => {
+    assert.strictEqual(LetterChanges('I Love New York'), 'G Mpw Zpsl');
+  });
 });
